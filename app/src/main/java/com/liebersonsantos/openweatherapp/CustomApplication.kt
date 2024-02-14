d03eec762d710c7697793a6e8efc09a6ed7d74d9
liebersonsantos <liebersonsantos.one@gmail.com>
2024-02-14 09:21:34 -0300

    config HttpClient

---
+++ app/src/main/java/com/liebersonsantos/openweatherapp/CustomApplication.kt
@@ -0,0 +1,8 @@
+package com.liebersonsantos.openweatherapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CustomApplication: Application() {
+}
